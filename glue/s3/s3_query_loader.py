fd860b2cbe0f6d6c2ad3897cdf23600530885748
mariana <mariana-medeiros@hotmail.com>
2024-08-03 20:34:45 -0300

    fix: adjust paths for glue job

--- glue/s3/s3_query_loader.py
+++ glue/s3/s3_query_loader.py
@@ -6,29 +6,29 @@ class QueryLoader:
         self.bucket = bucket
 
     def get_create_temp_table_query(self):
-        query = get_s3_obj_string(self.s3, self.bucket, 'glue/queries/create_temp_table.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, 'queries/create_temp_table.sql')
         return query
     
     def get_add_column_temp_table_query(self):
-        query = get_s3_obj_string(self.s3, self.bucket, 'glue/queries/add_column_temp_table.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, 'queries/add_column_temp_table.sql')
         return query
     
     def get_add_created_at_column_temp_table_query(self):
-        query = get_s3_obj_string(self.s3, self.bucket, 'glue/queries/add_created_at_column_temp_table.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, 'queries/add_created_at_column_temp_table.sql')
         return query
     
     def get_insert_temp_table_query(self):
-        query = get_s3_obj_string(self.s3, self.bucket, 'glue/queries/insert_temp_table.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, 'queries/insert_temp_table.sql')
         return query
     
     def get_general_insert_query(self):
-        query = get_s3_obj_string(self.s3, self.bucket, 'glue/queries/general_insert.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, 'queries/general_insert.sql')
         return query
     
     def get_specific_table_insert_query(self, table):
-        query = get_s3_obj_string(self.s3, self.bucket, f'glue/queries/{table}_table_insert.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, f'queries/{table}_table_insert.sql')
         return query
     
     def get_validate_query(self):
-        query = get_s3_obj_string(self.s3, self.bucket, 'glue/queries/validate.sql')
+        query = get_s3_obj_string(self.s3, self.bucket, 'queries/validate.sql')
         return query
